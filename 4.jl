d41848539d5c3ab52aff3d9d1f3b7c7ef49da961
ice <113590185+LondonDrip@users.noreply.github.com>
2022-12-26 01:34:53 +0300

    Update 4.jl

--- 4.jl
+++ 4.jl
@@ -1,34 +1,42 @@
 using HorizonSideRobots
-r=Robot(15,15; animate=true)
-function diagonal!(r)
-    for side in (West, Sud, Ost, Nord)
-        n = numsteps_putmarkers!(robot, side)
-        along!(robot, inverse(side), n)
+
+
+function cross_move(robot, side_x, side_y)
+    num_step = 0
+    while !isborder(robot, side_x) && !isborder(robot, side_y)
+        move!(robot, side_x)
+        move!(robot, side_y)
+        putmarker!(robot)
+        num_step += 1
+    end 
+
+    #блок инверсии
+    if side_x == Nord
+        side_x = Sud
+    else
+        side_x = Nord
+    end
+
+    if side_y == Ost
+        side_y = West
+    else
+        side_y = Ost
+    end
+    #
+
+    for _ in 1:num_step
+        move!(robot, side_x)
+        move!(robot, side_y)
+    end
+end
+
+function plus!(robot)
+    for side_x in (Nord, Sud)
+        for side_y in (Ost, West)
+            cross_move(robot, side_x, side_y)
+        end
     end
     putmarker!(robot)
 end
 
-function numsteps_putmarkers!(r, side)
-    num_steps=0
-    while !isborder(robot,side) && !isborder(robot, next(side))
-        move!(robot,side)
-        move!(robot,next(side))
-        num_steps += 1
-        putmarker!(robot)
-    end
-    return num_steps
-end
-
-function along!(r, side, num_steps)
-    for _i in 1:num_steps
-        move!(r,side)
-        move!(r,next(side))
-    end
-end
-
-inverse(side::HorizonSide) = HorizonSide((Int(side) +2)% 4)
-next(side::HorizonSide) = HorizonSide((Int(side) +1)% 4)
-diagonal!(r)
-numsteps_putmarkers!(r)   
-along!(r)
-    
+plus!(Robot("fr.sit", animate=true))
